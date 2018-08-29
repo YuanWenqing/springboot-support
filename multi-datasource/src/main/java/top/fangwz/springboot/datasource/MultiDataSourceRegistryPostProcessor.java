3d6596146edc1d5379ce425f17ace3fdd4d4021a
yuan <yuan@yzyw-inc.com>
2018-08-29 20:22:26 +0800

    MOD: comment

--- multi-datasource/src/main/java/top/fangwz/springboot/datasource/MultiDataSourceRegistryPostProcessor.java
+++ multi-datasource/src/main/java/top/fangwz/springboot/datasource/MultiDataSourceRegistryPostProcessor.java
@@ -2,6 +2,7 @@ package top.fangwz.springboot.datasource;
 
 import com.google.common.base.Suppliers;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
@@ -13,12 +14,17 @@ import javax.sql.DataSource;
 import java.util.Map;
 
 /**
+ * TODO: 用ConfigurationProperties的方式读取配置，注入在这里处理是不可行的
+ * ConfigurationProperties是在ConfigurationPropertiesBindingPostProcessor中才处理,
+ * 所以只能使用BeanPostProcessor的方式，且优先级低于ConfigurationPropertiesBindingPostProcessor
+ *
  * @author: yuanwq
  * @date: 2018/8/29
  */
 public class MultiDataSourceRegistryPostProcessor implements BeanDefinitionRegistryPostProcessor {
   private MultiDataSourceProperties properties;
 
+  @Autowired
   public void setProperties(MultiDataSourceProperties properties) {
     this.properties = properties;
   }
